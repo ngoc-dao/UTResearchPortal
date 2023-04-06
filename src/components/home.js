b99dac96e61804cb3ba5ff8322911183c6afeb34
Ahad Karedia <ahadkaredia@utexas.edu>
2023-04-06 12:46:40 -0500

    CSS updates to home and login

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,76 @@
 /* import statements */
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { Button, TextField } from '@mui/material';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Button, TextField } from "@mui/material";
+
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#f1f1f1",
+    padding: "2rem",
+  },
+  title: {
+    fontSize: "2rem",
+    fontWeight: "bold",
+    fontFamily: "sans-serif",
+    margin: 0,
+  },
+  subtitle: {
+    fontSize: "1rem",
+    fontStyle: "italic",
+    fontFamily: "sans-serif",
+    margin: "20px 0",
+  },
+  buttonContainer: {
+    display: "flex",
+    gap: "10px",
+  },
+  signUpContainer: {
+    display: "inline-flex",
+    flexDirection: "column",
+    padding: "20px",
+    fontFamily: "sans-serif",
+    backgroundColor: "#fff",
+    textAlign: "left",
+    position: "fixed",
+    right: 0,
+    marginRight: "1rem",
+    marginTop: "1rem",
+  },
+  signUpTitle: {
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+    marginBottom: "10px",
+  },
+  formField: {
+    marginBottom: "10px",
+  },
+  selectField: {
+    width: "100%",
+    marginBottom: "10px",
+    padding: "5px",
+    border: "1px solid black",
+    borderRadius: "5px",
+  },
+  button: {
+    display: "inline-block",
+    backgroundColor: "#d35400",
+    color: "#fff",
+    textTransform: "initial",
+    fontSize: "16px",
+    fontWeight: "bold",
+    borderRadius: "5px",
+    textDecoration: "none",
+    boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.3)",
+    transition: "all 0.3s ease",
+  },
+  error: {
+    color: "red",
+  },
+};
 
 const Home = () => {
   /* user data */
@@ -12,17 +80,17 @@ const Home = () => {
   const [eid, setEid] = useState();
   const [password, setPassword] = useState();
   const [userType, setUserType] = useState("Student");
-  const [error, setError] = useState(); 
+  const [error, setError] = useState();
 
-  const navigate = useNavigate();           /* used to navigate user upon login */
+  const navigate = useNavigate(); /* used to navigate user upon login */
 
   function studentLogin() {
-    navigate('/studentlogin');
-  };
+    navigate("/studentlogin");
+  }
 
   function facultyLogin() {
-    navigate('/facultylogin')
-  };
+    navigate("/facultylogin");
+  }
 
   function accountCreation() {
     const fn = fname;
@@ -38,104 +106,111 @@ const Home = () => {
     }
 
     if (ps.length < 6) {
-      setError("Please ensure your password is atleast 6 characters!")
+      setError("Please ensure your password is atleast 6 characters!");
       return;
     }
 
     const account = {
-      "first" : fn,
-      "last" : ln,
-      "email" : em,
-      "eid" : ei,
-      "password" : ps,
-      "usertype" : us,
-    }
+      first: fn,
+      last: ln,
+      email: em,
+      eid: ei,
+      password: ps,
+      usertype: us,
+    };
 
-    axios.post("/newaccount", account).then(
-      res => {
-        // set errors
-        if (res.data["error"] === true) {
-          setError("Please provide proper fields to create an account");
-          if ("message" in res.data) {
-            setError(res.data["message"]);
-          }
-        } else {
-          setError("");
-          // TODO - navigate to user dashboard OR ask user to sign in with new account
+    axios.post("/newaccount", account).then((res) => {
+      // set errors
+      if (res.data["error"] === true) {
+        setError("Please provide proper fields to create an account");
+        if ("message" in res.data) {
+          setError(res.data["message"]);
         }
+      } else {
+        setError("");
+        // TODO - navigate to user dashboard OR ask user to sign in with new account
       }
-    )
-  };
+    });
+  }
 
   return (
-    <div className="App">
-      <h1 class='text-4xl p-5 font-semibold'> Welcome to the UT Research Portal </h1>
-      <p class='text-1xl p-3 italic'> Sign in to the portal by first selecting whether you are a student or faculty member </p>
-      <Button
-        color='primary'
-        onClick={studentLogin}
-        variant={'contained'}
-      >
-        Student
-      </Button>
-      <Button
-        color='primary'
-        onClick={facultyLogin}
-        variant={'contained'}
-      >
-        Faculty
-      </Button>
-      <p></p>
-      <p class='p-3 italic'> If you do not have an account, please create one below </p>
-      <TextField 
-        label="First Name" 
-        variant="filled" 
-        onChange={(event) => setFName(event.target.value)}> 
-      </TextField>
-      <br></br>
-      <TextField 
-        label="Last Name" 
-        variant="filled" 
-        onChange={(event) => setLName(event.target.value)}> 
-      </TextField>
-      <br></br>
-      <TextField 
-        label="Email" 
-        variant="filled" 
-        onChange={(event) => setEmail(event.target.value)}> 
-      </TextField>
-      <br></br>
-      <TextField 
-        label="EID" 
-        variant="filled" 
-        onChange={(event) => setEid(event.target.value)}> 
-      </TextField>
-      <br></br>
-      <TextField 
-        label="Password" 
-        variant="filled" 
-        type="password" 
-        onChange={(event) => setPassword(event.target.value)}> 
-      </TextField>
-      <br></br>
-      <select
-        onChange={(event) => setUserType(event.target.value)}
-        class='border-black'
-      >
-        <option selected value="Student"> Student </option>
-        <option value="Faculty"> Faculty </option>
-      </select>
-      <p class='p-3'></p>
-      <Button
-        color='primary'
-        onClick={accountCreation}
-        variant={'contained'}
-      >
-        Submit
-      </Button>
-      <p></p>
-      <p class='p-3'> {error} </p>
-      <p></p>
+    <div>
+      <div style={styles.container}>
+        <h1 style={styles.title}>UT Research Portal</h1>
+        <div style={styles.buttonContainer}>
+          <Button
+            color="primary"
+            onClick={studentLogin}
+            variant="contained"
+            style={styles.button}
+          >
+            For Students
+          </Button>
+          <Button
+            color="primary"
+            onClick={facultyLogin}
+            variant="contained"
+            style={styles.button}
+          >
+            For Faculty
+          </Button>
+        </div>
+      </div>
+      {/* <p style={styles.subtitle}>
+        Sign in to the portal by first selecting whether you are a student or
+        faculty member
+      </p> */}
+      <div style={styles.signUpContainer}>
+        <h2 style={styles.signUpTitle}>New? Sign Up Below!</h2>
+        <TextField
+          label="First Name"
+          variant="filled"
+          style={styles.formField}
+          onChange={(event) => setFName(event.target.value)}
+        />
+        <TextField
+          label="Last Name"
+          variant="filled"
+          style={styles.formField}
+          onChange={(event) => setLName(event.target.value)}
+        />
+        <TextField
+          label="Email"
+          variant="filled"
+          style={styles.formField}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <TextField
+          label="EID"
+          variant="filled"
+          style={styles.formField}
+          onChange={(event) => setEid(event.target.value)}
+        />
+        <TextField
+          label="Password"
+          variant="filled"
+          type="password"
+          style={styles.formField}
+          oonChange={(event) => setPassword(event.target.value)}
+        />
+        <select
+          value={userType}
+          onChange={(event) => setUserType(event.target.value)}
+          style={styles.formField}
+        >
+          <option value="Student">Student</option>
+          <option value="Faculty">Faculty</option>
+        </select>
+        {error && <p style={styles.error}>{error}</p>}
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={accountCreation}
+          style={styles.button}
+        >
+          Create Account
+        </Button>
+      </div>
     </div>
   );
 };
--- src/components/studentlogin.js
+++ src/components/studentlogin.js
@@ -1,83 +1,133 @@
 /* import statements */
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { Button, TextField } from '@mui/material';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Button, TextField } from "@mui/material";
+
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  },
+  button: {
+    display: "inline-block",
+    backgroundColor: "#d35400",
+    color: "#fff",
+    textTransform: "initial",
+    fontSize: "16px",
+    fontWeight: "bold",
+    borderRadius: "5px",
+    textDecoration: "none",
+    boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.3)",
+    transition: "all 0.3s ease",
+    width:'100%'
+  },
+  button2: {
+    display: "inline-block",
+    backgroundColor: "#808080",
+    color: "#fff",
+    textTransform: "initial",
+    fontSize: "16px",
+    fontWeight: "bold",
+    borderRadius: "5px",
+    textDecoration: "none",
+    boxShadow: "2px 2px 5px rgba(0, 0, 0, 0.3)",
+    transition: "all 0.3s ease",
+    width:'100%',
+    marginTop:'.5rem'
+  },
+};
 
 const StudentLogin = (props) => {
-    /* state hooks for login */
-    const [eid, setEid] = useState();             /* value of entered eid */
-    const [password, setPassword] = useState();   /* value of entered password */
-    const [error, setError] = useState();         /* value of error message displayed to user */
-    const navigate = useNavigate();               /* used to navigate user upon login */
-  
-    /* function to try and log in the user */
-    function tryLogin() {
-      const ei = eid;
-      const ps = password;
+  /* state hooks for login */
+  const [eid, setEid] = useState(); /* value of entered eid */
+  const [password, setPassword] = useState(); /* value of entered password */
+  const [error, setError] =
+    useState(); /* value of error message displayed to user */
+  const navigate = useNavigate(); /* used to navigate user upon login */
 
-      if (!ei || !ps) {
-        setError("Please fill out EID and/or Password fields");
-        return;
-      }
+  /* function to try and log in the user */
+  function tryLogin() {
+    const ei = eid;
+    const ps = password;
 
-      const user = {
-        "eid" : ei,
-        "password" : ps,
-      }
+    if (!ei || !ps) {
+      setError("Please fill out EID and/or Password fields");
+      return;
+    }
 
-      axios.post("/studentlogin", user).then(
-        res => {
-          if (res.data["error"] === true) {
-            setError(res.data["message"]);
-          } else {
-            setError("");
-            // try this?
-            user["fname"] = res.data["fname"]
-            user["lname"] = res.data["lname"]
-            user["email"] = res.data["email"]
-            localStorage.setItem('token-info', JSON.stringify(user));
-            console.log("LOGIN SUCCESS");
-
-            // set user details
-            props.setUser(user);
-
-            // TODO - navigate to student dashboard
-            navigate('/studentdashboard');
-          }
-        }
-      )
+    const user = {
+      eid: ei,
+      password: ps,
     };
-  
-        return (
-          <div className="App">
-              <h1 class='text-4xl p-5 font-semibold'> Student Sign In </h1>
-              <p class='text-1xl p-3 italic'> If you have an account, please login here. </p>
-              
-              <TextField 
-                label="EID" 
-                variant="filled" 
-                onChange={(event) => setEid(event.target.value)}> 
-              </TextField>
-              <p></p>
-              <TextField 
-                label="Password" 
-                variant="filled" 
-                type="password" 
-                onChange={(event) => setPassword(event.target.value)}> 
-              </TextField>
-              <p class='p-3'></p>
-              <Button 
-                color='primary' 
-                onClick={tryLogin}
-                variant={'contained'}
-              > 
-                Submit 
-              </Button>
-              <p></p>
-              <p> {error} </p>
-          </div>
-        );
+
+    axios.post("/studentlogin", user).then((res) => {
+      if (res.data["error"] === true) {
+        setError(res.data["message"]);
+      } else {
+        setError("");
+        // try this?
+        user["fname"] = res.data["fname"];
+        user["lname"] = res.data["lname"];
+        user["email"] = res.data["email"];
+        localStorage.setItem("token-info", JSON.stringify(user));
+        console.log("LOGIN SUCCESS");
+
+        // set user details
+        props.setUser(user);
+
+        // TODO - navigate to student dashboard
+        navigate("/studentdashboard");
+      }
+    });
+  }
+
+  return (
+    <div className="App" style={styles.container}>
+      <div style={{ backgroundColor: "#fff", padding: "20px" }}>
+        <h1 class="text-4xl p-5 font-semibold"> Student Sign In </h1>
+        <p class="text-1xl p-3 italic">
+          {" "}
+          If you have a student account, please login here.{" "}
+        </p>
+        <TextField
+          label="EID"
+          variant="filled"
+          onChange={(event) => setEid(event.target.value)}
+          style={{width:'100%'}}
+        ></TextField>
+        <p></p>
+        <TextField
+          label="Password"
+          variant="filled"
+          type="password"
+          onChange={(event) => setPassword(event.target.value)}
+          style={{width:'100%'}}
+        ></TextField>
+        <p class="p-3"></p>
+        <Button
+          color="primary"
+          onClick={tryLogin}
+          variant={"contained"}
+          style={styles.button}
+        >
+          Submit
+        </Button>
+        <Button
+          color="primary"
+          onClick={() => navigate("/")}
+          variant={"contained"}
+          style={styles.button2}
+        >
+          Return to Home
+        </Button>
+        <p></p>
+        <p> {error} </p>
+      </div>
+    </div>
+  );
 };
 
 export default StudentLogin;
